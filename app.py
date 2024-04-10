94d65bb37fde3382e5819ca2b2ee154d1fb1f084
Brandon Supinski <66806979+bsupinski@users.noreply.github.com>
2024-04-10 16:42:56 -0400

    Added helpers and intro
    Created helper function to keep app.py cleaner. Added the intro messages and input request.

---
+++ app.py
@@ -0,0 +1,27 @@
+import constants
+from helpers import clean_list, enter_option
+
+player_list = constants.PLAYERS
+
+def introduction():
+    print("\n\nBasketball Team Stats Tool\n\n     --Menu--\n\n")
+    print("\nHere are your choices: \nA) Display Team stats\nB) Quit")
+
+
+def menu_choice():
+    user_menu_choice = enter_option()
+    print(user_menu_choice)
+    while user_menu_choice != "A" and user_menu_choice != "B":
+        user_menu_choice = enter_option()
+    if user_menu_choice == "B":
+        quit()
+
+
+def choose_team():
+    print("Choose which team's stats to view\n\nA) Panthers\nB) Bandits\nC) Warriors\n\n")
+    return enter_option()
+
+if __name__ == "__main__":
+    introduction()
+    menu_choice()
+    choose_team()
---
+++ helpers.py
@@ -0,0 +1,33 @@
+def enter_option():
+    return input("Enter an option:  ").upper()
+
+def change_to_bool(lst):
+    """Change the yes/no of 'experience' value to True/False"""
+    for player in lst:
+        if player["experience"] == "True":
+            player["experience"] = True
+        else:
+            player["experience"] = False
+
+
+def get_guardians(lst):
+    """Takes the 'guardians' value and changes it to a list of just full names"""
+    for player in lst:
+        player["guardians"] = player["guardians"].replace("and", "")
+        if "  " in player["guardians"]:
+            player["guardians"] = player["guardians"].split("  ")
+        else: 
+            player["guardians"] = [player["guardians"]]
+
+
+def change_to_int(lst):
+    """Change the height from a string to an integer"""
+    for player in lst:
+        player["height"] = int(player["height"].split(" ")[0])
+
+
+def clean_list(lst):
+    """Applies the seperate cleaning functions into one"""
+    change_to_bool(lst) 
+    get_guardians(lst)
+    change_to_int(lst)
