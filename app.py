54d5bbc1b1bce9cec21b116a66003e4fa5a3fcd6
Brandon Supinski <66806979+bsupinski@users.noreply.github.com>
2024-04-12 15:58:31 -0400

    added user replay
    user can now choose to view more stats

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ def menu_choice():
     print("\nHere are your choices: \nA) Display Team stats\nB) Show All Player\nC) Quit")
     user_menu_choice = enter_option()
     while user_menu_choice != "A" and user_menu_choice != "B" and user_menu_choice != "C":
+        print("You entered an invalid entry. Please choose one of the following \nA) Display Team stats\nB) Show All Player\nC) Quit")
         user_menu_choice = enter_option()
     if user_menu_choice == "B":
         display_players(constants.PLAYERS)
@@ -19,19 +20,29 @@ def menu_choice():
 
 
 def choose_team():
-    print("Choose which team's stats to view\n\n1) Panthers\n2) Bandits\n3) Warriors\n\n4)Quit\n")
+    print("Choose which team's stats to view\n\n1) Panthers\n2) Bandits\n3) Warriors\n4)Quit\n")
     user_menu_option = enter_option()
     while user_menu_option != "1" and user_menu_option != "2" and user_menu_option != "3" and user_menu_option != "4":
-        print(user_menu_option, type(user_menu_option))
+        print("""You entered an invalid entry. Please choose the following\n1) Panthers\n2) Bandits\n3) Warriors\n4) Quit\n""")
         user_menu_option = enter_option()
     user_menu_option = int(user_menu_option)
     if user_menu_option == 1 or user_menu_option == 2 or user_menu_option == 3:
         display_team_stats(constants.TEAMS[user_menu_option - 1], distrubuted_teams[user_menu_option - 1])
-    else:
+    elif user_menu_option == 4:
         quit()
 
+
+def replay():
+    users_input = input("If you would like choose more informaion to view hit 'Enter':   ")
+    if users_input == "":
+        menu_choice()
+        choose_team()
+        replay()
+    else:
+        quit()
 
 if __name__ == "__main__":
     introduction()
     menu_choice()
     choose_team()
+    replay()
